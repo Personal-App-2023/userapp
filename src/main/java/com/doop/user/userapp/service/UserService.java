1a746132731076d063d13ab1d1f8a6cecf6f7a5d
doaa <doaa.farouk07@gmail.com>
2024-09-03 14:26:50 +0300

    auth local user

--- src/main/java/com/doop/user/userapp/service/UserService.java
+++ src/main/java/com/doop/user/userapp/service/UserService.java
@@ -29,4 +29,35 @@ public class UserService {
         }
         return user.getUserId();
     }
+
+    public User authAndGetUser(String token)
+    {
+        AuthUser authUser=fetchUserByToken(token);
+        List<User> users=repo.findByEmail(authUser.getEmail());
+
+        User user=null;
+        if(users!=null && users.size()>0)
+        {
+            user=users.get(0);
+        }
+        else{
+            user=new User();
+            Integer userId=generator.generateNextUserId();
+            user.setUserId(userId);
+            user.setName(authUser.getName());
+            user.setEmail(authUser.getEmail());
+            user=repo.save(user);
+        }
+        return user;
+    }
+
+    private AuthUser fetchUserByToken(String token) {
+        String uri="https://www.googleapis.com/oauth2/v3/userinfo?access_token="+token;
+        HttpEntity<String> entity = new HttpEntity<>("body", createGAPIHeaders(token));
+        ResponseEntity<AuthUser> result= restTemplate.exchange(uri, HttpMethod.GET, entity, AuthUser.class);
+        return result.getBody();
+        /*return restTemplate.exchange
+                (uri, HttpMethod.GET, new HttpEntity<T>(createGAPIHeaders(token)), AuthUser.class);
+                */
+    }
 }
